6e00647623d1270f2ebec23020fc9915027523f9
sophian098 <sophianguyen416@gmail.com>
2025-02-15 19:04:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,40 @@ def cooking():
 @app.route('/')
 def index():
     return render_template('index.html')
+  
+@app.route('/signup')
+def signup():
+  return render_template('form.html')
+
+@app.route('/form', methods=['POST'])
+def form():
+    fname=request.form.get('fname')
+    lname=request.form.get('lname')
+    password=request.form.get('password')
+    
+    if len(password)<6:
+        message="Your password is too short! Make sure it is 6 characters or more"
+        return render_template('form.html', message=message)
+  
+    if not any(character.isupper() for character in password):
+        message="Your password does not have a capital letter! Make sure it contains a capital letter."
+        return render_template('form.html', message=message)
+  
+    if not any(character.islower() for character in password):
+        message="Your password does not have a lower-case letter! Make sure it contains a lower-caseletter."
+        return render_template('form.html', message=message)
+    else:
+      return redirect(url_for('submit', fname=fname, lname=lname))
+
+@app.route('/submit')
+def submit():
+    fname=request.args.get('fname')
+    lname=request.args.get('lname')
+    return render_template('submit.html', fname=fname, lname=lname)
+  
+@app.route('/hacks')
+def hacks():
+    return render_template('personality.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
